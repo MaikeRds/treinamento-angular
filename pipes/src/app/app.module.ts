5875f62049800793edaa1c06151b0ef7ca54d9fa
Maike Rodrigues <maikedev7@gmail.com>
2021-05-31 13:36:51 -0300

    pipes

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -1,20 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.component';
+import { CamelCasePipe } from './camel-case.pipe';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+import { SettingsService } from './settings.service';
+
+registerLocaleData(localePt, 'pt-BR');
 
 @NgModule({
   declarations: [
     AppComponent,
-    ExemplosPipesComponent
+    ExemplosPipesComponent,
+    CamelCasePipe
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+  // {
+  //   provide: LOCALE_ID,
+  //   useValue: 'pt-BR'
+  // },
+  SettingsService,
+  {
+    provide: LOCALE_ID,
+    deps:[SettingsService],
+    useFactory: (settingsService: SettingsService) => {
+      return settingsService.getLocale()
+    },
+  }
+],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
