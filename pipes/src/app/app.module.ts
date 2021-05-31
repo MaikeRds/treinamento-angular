6a5b9a9c89c857a87b2779754728cda860e0ac91
Maike Rodrigues <maikedev7@gmail.com>
2021-05-31 14:22:51 -0300

    Criando um Pipe "Impuro"

--- pipes/src/app/app.module.ts
+++ pipes/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { ExemplosPipesComponent } from './exemplos-pipes/exemplos-pipes.componen
 import { CamelCasePipe } from './camel-case.pipe';
 import { SettingsService } from './settings.service';
 import { FiltroArrayPipe } from './filtro-array.pipe';
+import { FiltroArrayImpuroPipe } from './filtro-array-impuro.pipe';
 
 registerLocaleData(localePt, 'pt-BR');
 
@@ -18,7 +19,8 @@ registerLocaleData(localePt, 'pt-BR');
     AppComponent,
     ExemplosPipesComponent,
     CamelCasePipe,
-    FiltroArrayPipe
+    FiltroArrayPipe,
+    FiltroArrayImpuroPipe
   ],
   imports: [
     BrowserModule,
---
+++ pipes/src/app/filtro-array-impuro.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe } from '@angular/core';
+import { FiltroArrayPipe } from './filtro-array.pipe';
+
+@Pipe({
+  name: 'filtroArrayImpuro',
+  pure: false
+})
+export class FiltroArrayImpuroPipe extends FiltroArrayPipe {
+
+}
