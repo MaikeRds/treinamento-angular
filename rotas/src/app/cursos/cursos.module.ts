ad2ff3d94a1e4764585da1d8ce99ea83445ea081
Maike Rodrigues <maikedev7@gmail.com>
2021-07-02 07:43:01 -0300

    Rotas: Criando um módulo de rotas de funcionalidade

--- rotas/src/app/cursos/cursos.module.ts
+++ rotas/src/app/cursos/cursos.module.ts
@@ -1,19 +1,19 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
 import { InputTextModule } from 'primeng/inputtext';
 import { ListboxModule } from 'primeng/listbox';
 import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 import { CursoNaoEncontradoComponent } from './curso-nao-encontrado/curso-nao-encontrado.component';
 import { CursosComponent } from './cursos.component';
+import { CursosRoutingModule } from './cursos.routing.module';
 import { CursosService } from './cursos.service';
 
 
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule,
+    CursosRoutingModule,
     InputTextModule,
     ButtonModule,
     ListboxModule
