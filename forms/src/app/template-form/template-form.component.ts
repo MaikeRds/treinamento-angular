cbbccebf0adc7bcacbeae374872255dbdee1b53d
Maike Rodrigues <maikedev7@gmail.com>
2021-07-17 08:54:53 -0300

    Aplicando CSS na validação dos campos

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
   
   usuario: any = {
-    nome: 'Maike',
-    email: 'maike@email.com'
+    nome: null,
+    email: null
   }
 
   constructor() { }
