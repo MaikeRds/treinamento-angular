67168d7191af52640419f7836c9172f9e45ab221
Maike Rodrigues <maikedev7@gmail.com>
2021-07-17 19:18:49 -0300

    Populando campos com setValue e patchValue (CEP)

--- forms/src/app/template-form/template-form.component.ts
+++ forms/src/app/template-form/template-form.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, NgForm } from '@angular/forms';
 import { map } from 'rxjs/operators'
 
 @Component({
@@ -21,7 +22,7 @@ export class TemplateFormComponent implements OnInit {
       cidade: '',
       estado: ''
     }
-  }
+  };  
 
   constructor(private http: HttpClient) { }
 
@@ -29,34 +30,75 @@ export class TemplateFormComponent implements OnInit {
   }
 
   onSubmit(form: any) {
-    console.log(form)
-    console.log(this.usuario)
+    console.log(form);
+    console.log(this.usuario);
   }
 
   verificaValidTouched(campo: any) {
-    return !campo.valid && campo.touched
+    return !campo.valid && campo.touched;
   }
 
-  consultaCEP(event: any) {
+  consultaCEP(event: any, form: any) {
     let cep = event.target.value as string;
 
     //Nova variável "cep" somente com dígitos.
     cep = cep.replace(/\D/g, '');
 
     //Verifica se campo cep possui valor informado.
-        if (cep != "") {
-          //Expressão regular para validar o CEP.
-          const validacep = /^[0-9]{8}$/;
+    if (cep != "") {
+      //Expressão regular para validar o CEP.
+      const validacep = /^[0-9]{8}$/;
 
-           //Valida o formato do CEP.
-           if(validacep.test(cep)) {
-              this.http.get(`https://viacep.com.br/ws/${cep}/json/`)
-              .subscribe((data) => {
-                console.log(data)
-              })
-           }
+      //Valida o formato do CEP.
+      if (validacep.test(cep)) {
+        this.resetaDadosForm(form);
+        this.http.get(`https://viacep.com.br/ws/${cep}/json/`)
+          .subscribe((dados) => {
+            this.popularDadosForm(dados, form);
+          })
+      }
 
-        }
+    }
+  }
+
+  popularDadosForm(dados: any, formulario: NgForm) {
+    // formulario.setValue({
+    //   nome: formulario.value.nome,
+    //   email: formulario.value.email,
+    //   endereco: {
+    //     cep: dados.cep,
+    //     numero: '',
+    //     complemento: '',
+    //     rua: dados.logradouro,
+    //     bairro: dados.bairro,
+    //     cidade: dados.localidade,
+    //     estado: dados.uf
+    //   }
+    // })
+
+    formulario.form.patchValue({
+      endereco: {
+        numero: '',
+        complemento: '',
+        rua: dados.logradouro,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    });
+  }
+
+  resetaDadosForm(formulario: NgForm){
+    formulario.form.patchValue({
+      endereco: {
+        numero: null,
+        complemento: null,
+        rua: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    });
   }
 
 }
