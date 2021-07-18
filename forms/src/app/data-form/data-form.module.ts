18f764d24c96e957c74a6f9dc5f69ecdd83c4a7e
Maike Rodrigues <maikedev7@gmail.com>
2021-07-18 06:12:08 -0300

    Formulários reativos: Configuração (Módulo e Componente)

--- forms/src/app/data-form/data-form.module.ts
+++ forms/src/app/data-form/data-form.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CardModule } from 'primeng/card';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
 import { DataFormComponent } from './data-form.component';
 
 
@@ -7,7 +12,11 @@ import { DataFormComponent } from './data-form.component';
 @NgModule({
   declarations: [DataFormComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    ReactiveFormsModule,
+    ButtonModule,
+    InputTextModule,
+    CardModule
   ]
 })
 export class DataFormModule { }
