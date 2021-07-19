b47eb9716cebb22248dd5314a7bf56e617de3689
Maike Rodrigues <maikedev7@gmail.com>
2021-07-19 11:23:32 -0300

    Form groups (agrupando dados)

--- forms/src/app/data-form/data-form.component.ts
+++ forms/src/app/data-form/data-form.component.ts
@@ -28,25 +28,27 @@ export class DataFormComponent implements OnInit {
     this.formulario = this.formBuilder.group({
       nome: [null, [Validators.required, Validators.minLength(3)]],
       email: [null, [Validators.required, Validators.email]],
-      cep: [null, [Validators.required]],
-      numero: [null, [Validators.required]],
-      complemento: [null, [Validators.required]],
-      rua: [null, [Validators.required]],
-      bairro: [null, [Validators.required]],
-      cidade: [null, [Validators.required]],
-      estado: [null, [Validators.required]],      
+      endereco: this.formBuilder.group({
+        cep: [null, [Validators.required]],
+        numero: [null, [Validators.required]],
+        complemento: [null, [Validators.required]],
+        rua: [null, [Validators.required]],
+        bairro: [null, [Validators.required]],
+        cidade: [null, [Validators.required]],
+        estado: [null, [Validators.required]],
+      }),
     })
 
     //[Validators.required, Validators.minLength(3), Validators.maxLength(20)]
   }
 
-  verificaValidTouched(campo: string): boolean {    
+  verificaValidTouched(campo: string): boolean {
     return !this.formulario.get(campo)?.valid && !!this.formulario.get(campo)?.touched;
   }
 
   verificarEmail(): boolean {
-    return !!this.formulario.get('email')?.hasError('email')    
-  } 
+    return !!this.formulario.get('email')?.hasError('email')
+  }
 
   onSubmit(): void {
     console.log(this.formulario)
