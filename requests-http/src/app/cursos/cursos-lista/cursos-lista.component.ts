c4124283e074e053b2fac0acd115f1fe1244ab6c
Maike Rodrigues <maikedev7@gmail.com>
2021-07-28 15:49:14 -0300

    Http GET + Pipe Async

--- requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
+++ requests-http/src/app/cursos/cursos-lista/cursos-lista.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LazyLoadEvent } from 'primeng/api';
+import { Observable } from 'rxjs';
+import { finalize, tap } from 'rxjs/operators';
 import { Curso } from '../curso';
 import { CursosService } from '../cursos.service';
 
@@ -10,11 +13,29 @@ import { CursosService } from '../cursos.service';
 export class CursosListaComponent implements OnInit {
 
   cursos: Curso[] = []
+  cursos$ = new Observable<any>()
+  loading: boolean = true;
 
   constructor(private service: CursosService) { }
 
   ngOnInit(): void {
-    this.service.list().subscribe(dados => this.cursos = dados)
+    this.service.list().subscribe(dados => { 
+      this.cursos = dados;
+      this.loading = false;
+    });
+    //this.cursos$ = this.service.list()
+
+  }
+
+  loadCursos(event: LazyLoadEvent) {
+    console.log(event)
+    this.loading = true;
+    console.log('aaa')
+
+  }
+
+  ngAfterViewChecked() {
+  
   }
 
 }
--- requests-http/src/app/cursos/cursos.service.ts
+++ requests-http/src/app/cursos/cursos.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { tap } from 'rxjs/operators'
+import { delay, tap } from 'rxjs/operators'
 import { environment } from 'src/environments/environment';
 
 import { Curso } from './curso';
@@ -16,7 +16,9 @@ export class CursosService {
 
   list() {
     return this.http.get<Curso[]>(this.API).pipe(
+      delay(2000),
       tap(console.log)
     )
   }
+
 }
