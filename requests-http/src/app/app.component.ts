c90773f4a491408606d8d82cfaa8656df190b6e4
Maike Rodrigues <maikedev7@gmail.com>
2021-07-28 08:35:02 -0300

    Adicionado template primeng

--- requests-http/src/app/app.component.ts
+++ requests-http/src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem, PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private primengConfig: PrimeNGConfig) { }
+
+  items: MenuItem[] = [];
   title = 'requests-http';
+
+  ngOnInit() {
+    this.primengConfig.ripple = true;
+
+    this.items = [
+      {
+        label: 'Angular Http',
+        icon: 'pi pi-fw pi-home',
+        routerLink: '/',
+        routerLinkActiveOptions: { exact: true }
+      },
+      {
+        label: 'Menu 01',
+        icon: 'pi pi-fw pi-file',
+        routerLink: '/menu01',
+        routerLinkActiveOptions: { exact: true }
+      },
+      {
+        label: 'Menu 02',
+        icon: 'pi pi-fw pi-file',
+        routerLink: '/menu02',
+        routerLinkActiveOptions: { exact: true }
+      }
+    ];
+  }
 }
--- requests-http/src/app/app.module.ts
+++ requests-http/src/app/app.module.ts
@@ -1,5 +1,11 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +16,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    ButtonModule,
+    InputTextModule,
+    MenubarModule,
     AppRoutingModule
   ],
   providers: [],
