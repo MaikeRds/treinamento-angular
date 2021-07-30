83f28fdb447cc73c7cd321c991143e197fa14cde
Maike Rodrigues <maikedev7@gmail.com>
2021-07-30 10:50:18 -0300

    Upload de Arquivo: Back-end com Node.js

---
+++ requests-http/server/src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors =  require('cors');
+const multiParty = require('connect-multiparty');
+
+const app = express();
+// parse application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: false }))
+
+app.use(express.json());
+
+const corsOptions = {
+  origin: '*',
+  optionSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
+
+const multipartyMiddleware = multiParty({ upload: './uploads' })
+
+app.post('/upload', multipartyMiddleware, (req, res) => {
+  const files = req.files;
+  console.log(files);
+
+  res.json({ message: files });
+});
+
+app.use((err, req, res, next) => res.json({ error: err.message }));
+
+app.listen(8000, () => {
+  console.log('Servidor iniciado. Porta: 8000');
+});
